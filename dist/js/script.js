ee9e170303db255645c7d25f5a4f77577f6c26a2
Edy Susilo <masedysu69@gmail.com>
2023-08-30 00:08:31 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,46 @@
+
+//navbar fixed
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixednav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
+
+
+    if(window.pageYOffset > fixednav) {
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    }else{
+        header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
+        toTop.classList.remove('flex');
+    }
+};
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+window.addEventListener('click', function(e) {
+    if(e.target != hamburger && e.target != navMenu){
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+    }
+});
+
+//Dark Mode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+    if(darkToggle.checked){
+        html.classList.add('dark');
+    }else{
+        html.classList.remove('dark');
+    }
+});
